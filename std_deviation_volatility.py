cb53ac16dbcf27ce4791b75167e079aaead4b778
OsaeAddo <immanueloaddo@gmail.com>
2022-08-13 21:52:21 +0000

    refactor volatility files

--- std_deviation_volatility.py
+++ std_deviation_volatility.py
@@ -1,7 +1,5 @@
 from ._base_data import *
 
-data = yf.download("AAPL", start="2017-01-01", end="2022-06-30")
-
 
 def standard_deviation(price_data, window=30, trading_periods=252, clean=True):
     log_return = (price_data["Close"] / price_data["Close"].shift(1)).apply(np.log)
