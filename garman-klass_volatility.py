c7c0113e626dfef8290db60ddb135da9f6d87ccb
OsaeAddo <immanueloaddo@gmail.com>
2022-08-17 22:18:35 +0000

    retrieve open & close price data

--- garman-klass_volatility.py
+++ garman-klass_volatility.py
@@ -3,3 +3,4 @@ from ._base_data import *
 
 def garman_klass(price_data, window=30, trading_periods=252, clean=True):
     log_hl = (price_data["High"] / price_data["Low"]).apply(np.log)
+    log_co = (price_data["Close"] / price_data["Open"]).apply(np.log)
