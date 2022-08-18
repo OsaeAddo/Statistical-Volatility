14b0d7b2ec12ebdbfc79afeeec92d64b78b0d5c3
OsaeAddo <immanueloaddo@gmail.com>
2022-08-18 13:11:20 +0000

    update function

--- garman-klass_volatility.py
+++ garman-klass_volatility.py
@@ -6,3 +6,8 @@ def garman_klass(price_data, window=30, trading_periods=252, clean=True):
     log_co = (price_data["Close"] / price_data["Open"]).apply(np.log)
     
     rs = 0.5 * log_hl ** 2 - (2 * math.log(2) - 1) * log_co ** 2
+    
+    def f(v):
+        return (trading_periods * v.mean()) ** 0.5
+    
+    result = rs.rolling(window=window, center=False)
