d0c8624df3c743bb641018e2eb042c93dff10f81
OsaeAddo <immanueloaddo@gmail.com>
2022-09-06 23:55:10 +0000

    update hodges-tompkins volatility

--- hodges-tompkins_volatility.py
+++ hodges-tompkins_volatility.py
@@ -2,4 +2,6 @@ from ._base_data import *
 
 
 def hodges_tompkins(price_data, window=30, trading_periods=252, clean=True):
-    pass
+    log_return = (
+        price_data["Close"] / price_data["Close"].shift(1).apply(np.log)
+    )
